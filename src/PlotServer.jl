1d5716c447e3c46a4179712cceeb62ed8a988283
Menyoung Lee <menyounglee@gmail.com>
2014-09-28 12:10:38 -0700

    moved some code between modules

--- src/PlotServer.jl
+++ src/PlotServer.jl
@@ -3,3 +3,23 @@
 # push data via WebSockets
 # for each WS client, first push all current data.
 # Then for each incoming data, push it to all clients at regular intervals.
+
+import HttpServer
+import WebSockets
+using JSON
+
+wsh = WebSockets.WebSocketHandler() do req,client
+		msg = WebSockets.read(client)
+		println(msg)
+		for (i,x,y) in Task(() -> produce_datum(ch0, ch1, x_itr, tstep))
+			data[i] = y
+			WebSockets.write(client, json((x,y)))
+        end
+    end
+
+server = HttpServer.Server(wsh)
+try
+	HttpServer.run(server,8080)
+catch
+	data
+end
--- src/Trace.jl
+++ src/Trace.jl
@@ -2,55 +2,28 @@
 # 1. feed data to a "plotting server" by raw TCP socket
 # 2. save data periodically into the filesystem
 # the plotting server caches all the data that should be live-plotted
+# the server: written in what language/framework? Could be Julia, Python, or Node.
 # the D3.js rendering page receives the data from the plotting server by WebSockets
 
-export produce_data, trace, traces
+export trace, traces
 
-import HttpServer
-import WebSockets
-using JSON
-
-# a producer function takes data
-function produce_datum(ch0::Output, ch1::Input, x_itr, tstep)
-	for (i,x) in enumerate(x_itr)
-		source(ch0, x)
-		sleep(tstep)
-		produce(i,x,measure(ch1))
-	end
-end
+# a producer function takes data: unnecessary in current framework
+# function produce_datum(ch0::Output, ch1::Input, x_itr, tstep)
+# 	for (i,x) in enumerate(x_itr)
+# 		source(ch0, x)
+# 		sleep(tstep)
+# 		produce(i,x,measure(ch1))
+# 	end
+# end
 
 function trace(ch0::Output, ch1::Input, x_itr, tstep, port)
+	# start the plot server, take data and feed to server
+	`julia PlotServer.jl`
 	map(x_itr) do x
 		source(ch0, x)
 		sleep(tstep)
 		measure(ch1)
 		feed_data()
-	end
-end
-
-function serve_data()
-# consumer function runs the display server
-	data = Array(Float64, length(x_itr))
-	for (i,x) in enumerate(x_itr)
-		source(ch0, x)
-		sleep(tstep)
-		data[i] = measure(ch1)
-	end
-	data
-	wsh = WebSockets.WebSocketHandler() do req,client
-			msg = WebSockets.read(client)
-			println(msg)
-			for (i,x,y) in Task(() -> produce_datum(ch0, ch1, x_itr, tstep))
-				data[i] = y
-				WebSockets.write(client, json((x,y)))
-	        end
-	    end
-
-	server = HttpServer.Server(wsh)
-	try
-		HttpServer.run(server,8080)
-	catch
-		data
 	end
 end
 
