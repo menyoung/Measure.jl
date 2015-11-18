29f146f4f127eedc099b3b80e0ba42ded4eec3f3
Menyoung Lee <menyounglee@gmail.com>
2015-11-17 20:54:56 -0800

    testing needs packages

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using Measure
 using Base.Test
 
+Pkg.clone("https://github.com/menyoung/VISA.jl")
+Pkg.clone("https://github.com/menyoung/Plotly.jl")
+Pkg.build("VISA")
+Pkg.build("Plotly")
+
 # write your own tests here
 @test 1 == 1
