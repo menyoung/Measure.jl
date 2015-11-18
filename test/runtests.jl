f3dc1432d10df3336b3e870d70815a4884d95701
Menyoung Lee <menyounglee@gmail.com>
2015-11-17 20:56:28 -0800

    nevermind

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,5 @@
 using Measure
 using Base.Test
 
-Pkg.clone("https://github.com/menyoung/VISA.jl")
-Pkg.clone("https://github.com/menyoung/Plotly.jl")
-Pkg.build("VISA")
-Pkg.build("Plotly")
-
 # write your own tests here
 @test 1 == 1
